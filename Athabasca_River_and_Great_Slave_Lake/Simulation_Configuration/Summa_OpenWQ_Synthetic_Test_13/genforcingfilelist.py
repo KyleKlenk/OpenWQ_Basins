6856440e939fa06314b0a7a017a0ed154b007e28
KyleKlenk <kyle.c.klenk@gmail.com>
2024-01-09 18:12:33 -0600

    Add Great Slave Lake configuration for synthetic test 8

--- Athabasca_River_and_Great_Slave_Lake/Simulation_Configuration/Summa_OpenWQ_Synthetic_Test_13/genforcingfilelist.py
+++ Athabasca_River_and_Great_Slave_Lake/Simulation_Configuration/Summa_OpenWQ_Synthetic_Test_13/genforcingfilelist.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timedelta
 
-start_date = datetime(2001, 3, 1)
-end_date = datetime(2022, 12, 1)
+start_date = datetime(2000, 1, 1)
+end_date = datetime(2005, 12, 1)
 
 current_date = start_date
 file_list = []
 
 while current_date <= end_date:
-  filename = f"Great_Slave_Lake_Basin_remapped_{current_date.strftime('%Y-%m-%d-%H-%M-%S')}.nc"
+  filename = f"Great_Slave_Lake_Basin_remapped_{current_date.strftime('%Y-%m-01-%H-%M-%S')}.nc"
   file_list.append(filename)
   current_date += timedelta(days=31)  # increment by one month approximately
 
